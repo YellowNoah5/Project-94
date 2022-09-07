37820205f6cc48229b0e41db1326ae2ad74abff7
YellowNoah5 <103080637+YellowNoah5@users.noreply.github.com>
2022-09-06 20:31:37 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDiNYzbP-WZ34NatiTySWMDLA4NV_QDJtc",
+    authDomain: "quacker-b5cf5.firebaseapp.com",
+    projectId: "quacker-b5cf5",
+    storageBucket: "quacker-b5cf5.appspot.com",
+    messagingSenderId: "38689050446",
+    appId: "1:38689050446:web:f1f66ddb2c4fbd7fecd81f"
+  };
